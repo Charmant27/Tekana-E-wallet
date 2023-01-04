bd97cf3142c5903ae01579db9ede5b5a8df816c6
charmant27 <charmantogatera@gmail.com>
2023-01-03 21:42:43 -0800

    auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,17 +3,17 @@ import { Controller, Body, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto, SigninDto } from './dto';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('api/signup')
+  @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
   @HttpCode(HttpStatus.OK)
-  @Post('api/signin')
+  @Post('signin')
   signin(@Body() dto: SigninDto) {
     return this.authService.signin(dto);
   }
