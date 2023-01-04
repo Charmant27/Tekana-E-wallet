b1e3e2d1f62b1612808825cb14e3a60a7c07d5b6
charmant27 <charmantogatera@gmail.com>
2023-01-03 21:45:07 -0800

    user routes

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common/pipes';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditWalletDto, WalletDto } from './dto';
+import { WalletService } from './wallet.service';
 
-@Controller('wallet')
-export class WalletController {}
+@UseGuards(JwtGuard)
+@Controller('api/wallets')
+export class WalletController {
+  constructor(private walletService: WalletService) {}
+
+  @Get()
+  getAllWallets(@GetUser('id') userId: number) {
+    return this.walletService.getAllWallets(userId);
+  }
+
+  @Get(':id')
+  getSingleWallet(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) walletId: number,
+  ) {
+    return this.walletService.getSingleWallet(userId, walletId);
+  }
+
+  @Post('wallet')
+  createWallet(@GetUser('id') userId: number, @Body() dto: WalletDto) {
+    return this.walletService.createWallet(userId, dto);
+  }
+
+  @Patch(':id')
+  updateWallet(
+    @GetUser('id') userId: number,
+    @Body() dto: EditWalletDto,
+    @Param('id', ParseIntPipe) walletId: number,
+  ) {
+    return this.walletService.updateWallet(userId, dto, walletId);
+  }
+
+  @Delete(':id')
+  deleteWallet(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) walletId: number,
+  ) {
+    return this.walletService.deleteWallet(userId, walletId);
+  }
+}
