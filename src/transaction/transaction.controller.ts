ce9e09f272939710b014e6a81f6a8c8d984a1129
charmant27 <charmantogatera@gmail.com>
2023-01-07 12:57:43 -0800

    transaction routes

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Get,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto';
 
-@Controller('transaction')
-export class TransactionController {}
+@UseGuards(JwtGuard)
+@Controller('api/transactions')
+export class TransactionController {
+  constructor(private transactionService: TransactionService) {}
+
+  @Post('transaction')
+  createTransaction(@Body() dto: CreateTransactionDto) {
+    return this.transactionService.createTransaction(dto);
+  }
+
+  @Get()
+  viewAllTransactionsMade() {
+    return this.transactionService.viewAllTransactionsMade();
+  }
+
+  @Get(':id')
+  viewSingleTransaction(@Param('id', ParseIntPipe) transactionId: number) {
+    return this.transactionService.viewSingleTransaction(transactionId);
+  }
+}
